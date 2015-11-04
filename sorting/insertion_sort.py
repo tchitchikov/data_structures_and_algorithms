c3c98bab3f06748d06249dedcd4e1bcb93efc99b
tchitchikov <jinterlan5@gmail.com>
2015-11-04 00:01:50 -0500

    finished insertion sort

--- sorting/insertion_sort.py
+++ sorting/insertion_sort.py
@@ -5,7 +5,7 @@ The first value is inserted into the sorted portion, all others remain in
 the unsorted portion. The next value is selected from the unsorted array and
 compared against each value in the sorted array and inserted in its proper
 ascending or descending order depending on how you code this.
-Run all values are in the sorted array.
+Run so all values are in the sorted array.
 """
 from base_class import Sort
 
@@ -13,32 +13,18 @@ from base_class import Sort
 class InsertSort(Sort):
     def sorting_process(self, input_array):
         """
-        :return:
+        Uses only a single list and compares the two indexed values
         """
         unsorted = input_array
-        sorted_array = [input_array[0]]
-        # unsorted_array.pop(0)  # look into making this a collections.deque object instead of a list
-        # `i = 0
-        # while i < len(unsorted_array):
-        #     element = sorted_array[i]
-        #     j = i + 1
-        #     while ((j > 0) and (sorted_array[j-1] > element)):
-        #         # sorted_array[j] = sorted_array[j-1]
-        #         j = j - 1
-        #     sorted_array.insert(j, element)
-        #     i = i + 1
-        #     print( i , j , element)`
-        # return sorted_array
-        i = 1
-        while i < (len(unsorted)-1):
-            x = unsorted[i]
-            j = i
-            while((j > 0) and (unsorted[j-1] > x)):
-                unsorted[j] = unsorted[j-1]
-                j = j -1
-            unsorted[j] = x
-            i = i + 1
+        for index in range(1, len(unsorted)):
+            value = unsorted[index]
+            index_2 = index - 1
+            while(index_2 >= 0 and unsorted[index_2] > value):
+                unsorted[index_2+1] = unsorted[index_2]  # shift number in slot i to slot i + 1
+                unsorted[index_2] = value  # shift value left into slot i
+                index_2 = index_2-1
         return unsorted
+
 
 
 if __name__ == '__main__':
